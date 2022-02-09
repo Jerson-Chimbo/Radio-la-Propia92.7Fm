168d581a423f8de06af3181817fb56a202fe5faa
Jerson-chimbo <jerson.chimbo2000@gmail.com>
2022-02-09 13:09:14 -0500

    banner adjunto

--- views/client/home.html.php
+++ views/client/home.html.php
@@ -1,4 +1,15 @@
-<div class="banner"></div>
+<div class="banner">
+ <div class="capa"></div>
+		<div class="info">
+			<a href="#">Leer Màs</a>
+		</div> 
+        
+        <!-- <h1>Bienvenidos a la Radio la Propia 92.7FM</h1>
+			<p>Somos la mejor radio a nivel nacional con las mejores noticias, deportes y mucho mas.</p> -->
+</div>
+		
+	</div>
+
                 <div class="transmicion">
                     <h2>Transmición<span> en  vivo</span></h2>
                     <div class="reproductor"></div>
