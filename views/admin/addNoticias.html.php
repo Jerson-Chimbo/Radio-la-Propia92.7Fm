809c3439837a0b1a9e8ee32e26e60b6f558c1e2a
Jerson-chimbo <jerson.chimbo2000@gmail.com>
2022-02-09 22:26:36 -0500

    ingreso de noticias

--- views/admin/addNoticias.html.php
+++ views/admin/addNoticias.html.php
@@ -1,1 +1,11 @@
-noticasia
+<div class="formulario1">
+<form action="" method="post">
+        <label for="titulo">Ingrese el Titulo de la Noticia</label>
+        <input type="text" name="Titulo" >
+        <label for="Imagen">Ingrese la Imagen de la Noticia</label>
+        <input type="file" name="imagen" />
+        <label for="descripcion">Ingrese la Descripcion de la Noticia</label>
+        <textarea name="descripcion"></textarea>
+        <button type="submit">Guardar</button>
+    </form>
+</div>
